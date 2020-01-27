8d807d0ae6b5bc918c18b572831ac78b89d31430
Kyle Leonard <kdleonard@icloud.com>
2020-01-26 21:02:03 -0600

    progress with color list

--- client/src/components/ColorList.js
+++ client/src/components/ColorList.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import axios from "axios";
+import axiosWithAuth from "../utils/api";
 
 const initialColor = {
   color: "",
@@ -21,10 +21,26 @@ const ColorList = ({ colors, updateColors }) => {
     // Make a put request to save your updated color
     // think about where will you get the id from...
     // where is is saved right now?
+    axiosWithAuth()
+      .put(`/colors/${colorToEdit.id}`, colorToEdit)
+      .then(res => {
+        updateColors([
+          ...colors.filter(color => color.id !== colorToEdit.id),
+          res.data
+        ]);
+        setEditing(false);
+      })
+      .catch(error => console.log("Error:", error));
   };
 
   const deleteColor = color => {
     // make a delete request to delete this color
+    axiosWithAuth()
+      .delete(`http://localhost:5000/api/colors/${removeColor.id}`)
+      .then(() => {
+        updateColors(colors.filter(color => color.id !== removeColor.id));
+      })
+      .catch(error => console.log("Error:", error));
   };
 
   return (
@@ -34,12 +50,14 @@ const ColorList = ({ colors, updateColors }) => {
         {colors.map(color => (
           <li key={color.color} onClick={() => editColor(color)}>
             <span>
-              <span className="delete" onClick={e => {
-                    e.stopPropagation();
-                    deleteColor(color)
-                  }
-                }>
-                  x
+              <span
+                className="delete"
+                onClick={e => {
+                  e.stopPropagation();
+                  deleteColor(color);
+                }}
+              >
+                x
               </span>{" "}
               {color.color}
             </span>
--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -55,7 +55,7 @@ const Login = props => {
           onChange={handleChange}
         />
 
-        <button className="login-btn"> Log In </button>
+        <button> Log In </button>
       </form>
     </>
   );
